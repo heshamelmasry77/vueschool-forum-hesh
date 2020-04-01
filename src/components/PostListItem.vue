872ae4bda994e0a50cbfbd1de85aec1cfdcb1d0a
hesham <heshamelmasry1992@gmail.com>
2020-04-01 21:15:25 +0200

    use this.set and add post

---
+++ src/components/PostListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{user.name}}</a>
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+      </a>
+      <p class="desktop-only text-small">{{userPostsCount}}</p>
+    </div>
+
+    <div class="post-content">
+      <div>{{post.text}}</div>
+    </div>
+    <div class="post-date text-faded">
+      {{post.publishedAt}}
+    </div>
+  </div>
+</template>
+<script>
+  import sourceData from '@/data.json'
+
+  export default {
+    props: {
+      post: {
+        required: true,
+        type: Object
+      }
+    },
+    computed: { // computed transform data
+      user () {
+        return sourceData.users[this.post.userId]
+      },
+      userPostsCount () {
+        const postIds = this.user.posts // get a the post Ids
+        const postIdArray = Object.keys(postIds) // create an array hold all the posts
+        return postIdArray.length - 1 // get the number of posts the user written
+      }
+    }
+  }
+</script>
