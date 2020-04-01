adea738f9955247e440190276cb15d433a21545e
hesham <heshamelmasry1992@gmail.com>
2020-04-01 14:21:07 +0200

    use modules in css and add new not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 :class="$style.headingLarge">Not Found Page</h1>
+    <p :class="$style['text-large']">oops!! </p>
+    <router-link :to="{name : 'PageHome'}">Go back to Home Page</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--using module css with classes-->
+<style module>
+  .headingLarge {
+    font-size: 50px;
+  }
+  .text-large {
+    font-size: 100px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import PageHome from '../pages/PageHome'
 import PageThreadShow from '../pages/PageThreadShow'
+import NotFound from '../pages/NotFound'
 
 Vue.use(Router) // tell vue to use the router plugin
 
@@ -17,6 +18,12 @@ export default new Router({ // route options
       name: 'PageThreadShow',
       component: PageThreadShow,
       props: true // pass params to the component as properties
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      // redirect: {name: 'PageHome'} // named route we can also do redirect : PageHome
+      component: NotFound
     }
   ],
   mode: 'history' // to remove the #
